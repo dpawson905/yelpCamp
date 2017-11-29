0a1dc71bab2598116e6f12edd90a0a8456709d4c
Darrell Pawson <dpawson905@gmail.com>
2017-11-29 22:49:01 +0000

    add fuzzy search for campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -9,14 +9,29 @@ var geocoder = require('geocoder');
 
 // this sets access to /campgrounds
 router.get("/", function(req, res){
-    // get all campgrounds from DB
-    Campground.find({}, function(err, allCampgrounds){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("campgrounds/index", {campgrounds: allCampgrounds});
-        } 
-    });
+    var noMatch = null;
+    if(req.query.search) {
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        Campground.find({name: regex}, function(err, allCampgrounds){
+            if(err){
+                console.log(err);
+            } else {
+                if(allCampgrounds.length < 1) {
+                    noMatch = "No campgrounds match that query, please try again.";
+                } 
+                res.render("campgrounds/index", {campgrounds: allCampgrounds, noMatch: noMatch});
+            } 
+        });
+    } else {
+        // get all campgrounds from DB
+        Campground.find({}, function(err, allCampgrounds){
+            if(err){
+                console.log(err);
+            } else {
+                res.render("campgrounds/index", {campgrounds: allCampgrounds, noMatch: noMatch});
+            } 
+        });
+    }
 });
 
 //CREATE - add new campground to DB
@@ -109,6 +124,10 @@ router.delete("/:id", middleware.checkCampgroundOwnership,  function(req, res){
         }
     });
 });
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
 
 
 module.exports = router;
